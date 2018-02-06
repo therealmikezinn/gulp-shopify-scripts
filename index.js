f00f59c2ac2fc636f6b586977c4b3e717729db45
Michael Zinn <zinnberg@gmail.com>
2018-02-06 10:49:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const through2 = require('through2');
+
+const Shopify = require('shopify-api-node');
+
+module.exports = function(options){
+    const API = new Shopify({
+        shopName: options.shopName,
+        apiKey: options.apiKey,
+        password: options.password,
+    });
+
+    return {
+        upload(){
+            return through2.obj(function (file, enc, cb) {
+                const self = this;
+
+                const { Location } = file;
+
+                API.scriptTag.create({
+                    "event": "onload",
+                    "src": Location,
+                }).then(result => {
+                    self.push(result);
+                    cb();
+                }).catch(err => {
+                    cb(err);
+                });
+            });
+        },
+        remove() {
+            return through2.obj(function(id, enc, cb){
+                const self = this;
+
+                API.scriptTag.delete({
+                    id
+                }).then(result => {
+                    self.push(result);
+                    cb();
+                }).catch(err => {
+                    cb(err);
+                });
+            });
+        }
+    };
+};
